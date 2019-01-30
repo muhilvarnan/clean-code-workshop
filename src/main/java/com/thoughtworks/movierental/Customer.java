15878831c1b67b1b55dff42fa6029b0019d8afeb
muhilvarnan <muhilvarnan.v@gmail.com>
2019-01-30 18:27:46 +0530

    getTiltle and getRentals refractor

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -19,28 +19,36 @@ public class Customer {
     return name;
   }
 
+  private String getTitle() {
+    return  "Rental Record for " + getName() + "\n";
+  }
+
+  private double getRentalAmount(Rental rental) {
+    double amount = 0;
+    switch (rental.getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        amount += 2;
+        if (rental.getDaysRented() > 2)
+          amount += (rental.getDaysRented() - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        amount += rental.getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        amount +=  1.5;
+        if (rental.getDaysRented() > 3)
+          amount += (rental.getDaysRented() - 3) * 1.5;
+        break;
+    }
+    return amount;
+  }
+
   public String statement() {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    String result = "Rental Record for " + getName() + "\n";
+    String result = getTitle();
     for (Rental each : rentals) {
-      double thisAmount = 0;
-      //determine amounts for each line
-      switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-      }
+      double thisAmount = getRentalAmount(each);
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
